750152cc6cefc82eb59d95de5a1cc101b3a9cc0f
Lingfei <lingfei@tju.edu.cn>
2025-01-21 13:33:06 +0800

    1

--- rebuild_and_reinstall.cmd
+++ rebuild_and_reinstall.cmd
@@ -1,3 +1,3 @@
 cd /d "%~dp0"
 python setup.py bdist_wheel
-pip install --no-index .\dist\easyjson-1.0.0-py3-none-any.whl --force-reinstall
+pip install --no-index .\dist\easy_json-1.0.0-py3-none-any.whl --force-reinstall
